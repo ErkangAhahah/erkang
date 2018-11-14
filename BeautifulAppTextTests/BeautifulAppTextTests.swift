cbfd9b794229f54e793e5ea0fc29e3ea0ac144a4
周杰 <quanshijiezuimeilideerkang@quanshijiezuimeilideerkangdemacbook-pro.local>
2018-11-14 17:22:27 +0800

    Initial Commit

---
+++ BeautifulAppTextTests/BeautifulAppTextTests.swift
@@ -0,0 +1,34 @@
+//
+//  BeautifulAppTextTests.swift
+//  BeautifulAppTextTests
+//
+//  Created by 全世界最美丽的尔康 on 2018/11/14.
+//  Copyright © 2018年 全世界最美丽的尔康. All rights reserved.
+//
+
+import XCTest
+@testable import BeautifulAppText
+
+class BeautifulAppTextTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
